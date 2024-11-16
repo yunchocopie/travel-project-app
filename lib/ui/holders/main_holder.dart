3fc374656f9a88bf99301afdb220dfe623f3ba2b
yunchocopie <yunnn687@naver.com>
2024-11-16 19:21:23 +0900

    design : BottomNavigationBar 추가 (홈/동행찾기/프로필)

--- lib/ui/holders/main_holder.dart
+++ lib/ui/holders/main_holder.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:team_project/ui/holders/home/home_page.dart';
+import 'package:team_project/ui/holders/finding/finding_page.dart';
+import 'package:team_project/ui/holders/profile/profile_page.dart';
+import 'home/home_page.dart';
 
 class MainHolder extends StatefulWidget {
   const MainHolder({super.key});
@@ -9,10 +12,46 @@ class MainHolder extends StatefulWidget {
 }
 
 class _MainHolderState extends State<MainHolder> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomePage(),
+      backgroundColor: Colors.white,
+      body: IndexedStack(
+        index: _index,
+        children: [
+          HomePage(),
+          FindingPage(),
+          ProfilePage()
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.green,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.zoom_in),
+            label: '동행 찾기',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            label: '프로필',
+          ),
+        ],
+        onTap: (value) {
+          setState(() {
+            _index = value;
+            print("클릭됨 ${value}");
+          });
+        },
+        currentIndex: _index,
+      ),
     );
   }
 }
